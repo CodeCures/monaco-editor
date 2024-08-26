96dfd5a4163485f12709199129ab68b9d2481aba
CodeCures <couragesblog@gmail.com>
2024-08-26 11:48:32 +0100

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/monaco-editor/',
-  publicDir: '/monaco-editor/',
+  base: '/monaco-editor',
+  publicDir: '/monaco-editor',
   plugins: [
     vue(),
   ],
